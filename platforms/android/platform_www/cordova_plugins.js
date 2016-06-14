754bd3d082a47f3fa8fb40420f5db2e95276cda7
unknown <AP47178@MOBW41L0037.ind.zensar.com>
2016-06-14 16:54:47 +0530

    initial commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-sqlite-storage": "1.4.1",
+    "cordova-plugin-whitelist": "1.2.2"
+};
+// BOTTOM OF METADATA
+});
